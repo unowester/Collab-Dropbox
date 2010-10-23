a41070c5e6264f2ea1bcc4a513b2acc1f51aaa6b
Anton Berner <antonberner@Anton-Berners-MacBook-Pro.local>
2010-10-23 23:18:56 +0200

    check the os and if  growl is on

--- src/se/webbninja/collabdropbox/Main.java
+++ src/se/webbninja/collabdropbox/Main.java
@@ -3,6 +3,30 @@ package se.webbninja.collabdropbox;
 public class Main {
   public static void main(String[] args) {
     System.out.println("HELLO WORLD");
+
+    selectNotifyer("file","name");
+   
+  }
+
+  public static void selectNotifyer(String file, String username){
+   
+    GrowlNotification gn = new GrowlNotification();
+    SwingNotification sw = new SwingNotification();
+    String os = System.getProperty("os.name");
+    
+    if ( os == null ? "Mac OS X" == null : os.equals("Mac OS X")){
+        if (gn.isRunning()){
+            gn.isUsingFile(file, username);
+        }
+        else {
+           sw.isUsingFile(file, username);
+        }
+    }
+    else{
+        sw.isUsingFile(file, username);
+    }
+
+
   }
 }
 
